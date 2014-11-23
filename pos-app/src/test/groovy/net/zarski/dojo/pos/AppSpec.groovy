10b85b0dda25c7c025aad70889d09f595d9cfc6c
Wojciech Żarski <wojciech.zarski@gmail.com>
2014-11-23 13:22:24 +0100

    Add spock testing skeleton

---
+++ pos-app/src/test/groovy/net/zarski/dojo/pos/AppSpec.groovy
@@ -0,0 +1,15 @@
+import spock.lang.*
+
+class AppSpec extends Specification {
+    @Unroll
+    def "minimum of #a and #b is #c"() {
+        expect:
+        Math.min(a, b) == c
+
+        where:
+        a | b || c
+        3 | 7 || 3
+        5 | 4 || 4
+        9 | 9 || 9
+    }
+}
