9bf6c3b306cb5482204bd7e48cec514442632bc1
Wojciech Zarski <wojciech.zarski@gmail.com>
2017-08-18 15:29:39 +0200

    Initialize project with Spring-Boot hello world

---
+++ 2017/webstore/src/main/java/net/zarski/dojo/webstore/hello/Application.java
@@ -0,0 +1,18 @@
+package net.zarski.dojo.webstore.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lb_lb on 18.08.17.
+ */
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
---
+++ 2017/webstore/src/main/java/net/zarski/dojo/webstore/hello/SampleController.java
@@ -0,0 +1,25 @@
+package net.zarski.dojo.webstore.hello;
+
+/**
+ * Created by lb_lb on 18.08.17.
+ */
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
