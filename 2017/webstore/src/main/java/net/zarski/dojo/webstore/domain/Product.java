642b62210dfb09080ea55f0404cd39a19af640d1
Wojciech Zarski <wojciech.zarski@gmail.com>
2017-08-19 16:00:33 +0200

    Refactoring of Functional Test and introducing own equal method

--- 2017/webstore/src/main/java/net/zarski/dojo/webstore/domain/Product.java
+++ 2017/webstore/src/main/java/net/zarski/dojo/webstore/domain/Product.java
@@ -26,11 +26,45 @@ public class Product implements Serializable {
     @Column(nullable = false)
     private String name;
 
+    public Product() {
+    }
+
+    public Product(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (id != null ? !id.equals(product.id) : product.id != null) return false;
+        return name != null ? name.equals(product.name) : product.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
