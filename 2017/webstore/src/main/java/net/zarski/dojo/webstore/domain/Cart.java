0f944bdee0b204a22e49027a909430103f38e8b3
Wojciech Zarski <wojciech.zarski@gmail.com>
2017-08-20 22:01:26 +0200

    Adds storing new Carts in database.

--- 2017/webstore/src/main/java/net/zarski/dojo/webstore/domain/Cart.java
+++ 2017/webstore/src/main/java/net/zarski/dojo/webstore/domain/Cart.java
@@ -1,10 +1,32 @@
 package net.zarski.dojo.webstore.domain;
 
-public class Cart {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Cart implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
     private String sessionId;
+
 
     public Cart(String sessionId) {
         this.sessionId = sessionId;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getSessionId() {
